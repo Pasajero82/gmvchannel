b70e8d4fd255d84154ef7014f9003e497d045fc5
Pasajero82 <nomegusta@hotmail.com>
2026-02-03 10:53:58 -0600

    Add files via upload

--- news.js
+++ news.js
@@ -2,9 +2,9 @@
 // Usa <span>Texto</span> para que salga en ROSA NEÓN.
 const newsItems = [
     "<span>🎧 GMV THE FUTURE GETS LOUD</span>",
-    "<span>GMV:</span> If you're an AI artist, this is your space.",
+    "If you're an AI artist, this is your space.",
     "The Mannequins will premiere their music video 📺'Dollhouse' soon on GMV",
-    "<span>📺 New video: NEXO - Quantum Leap</span> arrives tomorrow",
+    "<span>📺 New video: NEXO - Neural Network feat. LEXA</span> arrives tomorrow",
     "24 music playback",
     "<span>Press number 8 to access THE WALL. Press 7 to leave your message (Limited time only).</span>",
     "<span>Subscribe</span>to the YouTube channel to stay up to date with the latest news",
