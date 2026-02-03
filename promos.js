493e65ce72ab12d967b8fe273e5d01ff08388eb0
Pasajero82 <nomegusta@hotmail.com>
2026-02-03 10:56:22 -0600

    Add files via upload

--- promos.js
+++ promos.js
@@ -4,24 +4,24 @@ const promoItems = [
         // Ejemplo 1: Promoción de Spotify
         // Usa imágenes cuadradas (1:1) para mejor resultado
         img: "img/promo1.webp", 
-        title: "THE MANNEQUINS",
+        title: "XTHE MANNEQUINS",
         msg: "New single available on Spotify."
     },
     {
         // Ejemplo 2: Noticia del canal
         img: "img/promo2.webp",
-        title: "GENERATIVE MUSIC VIDEO YOUTUBE CHANNEL",
+        title: "XGENERATIVE MUSIC VIDEO YOUTUBE CHANNEL",
         msg: "Subscribe for more"
     },
     {
         // Ejemplo 3: Merch o Serie
         img: "img/promo3.webp",
-        title: "GUSTAVO CERATI IA",
+        title: "XGUSTAVO CERATI IA",
         msg: "New video 'Nada es para siempre' available"
     },
 {
         img: "img/promo4.webp",
-        title: "NEXO - Ghost In The System",
+        title: "XNEXO - Neural Network feat. LEXA",
         msg: "NOW AVAILABLE"
     }
 ];
