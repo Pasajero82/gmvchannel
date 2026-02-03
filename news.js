38ca0fc6acb0a01bcb758d85c275a4dc8a99d750
Pasajero82 <nomegusta@hotmail.com>
2026-02-03 10:39:34 -0600

    Add files via upload

--- news.js
+++ news.js
@@ -3,9 +3,9 @@
 const newsItems = [
     "<span>🎧 GMV THE FUTURE GETS LOUD</span>",
     "<span>GMV:</span> If you're an AI artist, this is your space.",
-    "The Mannequins will premiere their music video 📺 'Dollhouse' soon on GMV",
-    "<span>📺 New video: NEXO - Quantum Leap</span> arrives tomorrow",
-    "24/7 music playback",
+    "The Mannequins will premiere their music video 📺'Dollhouse' soon on GMV",
+    "<span>📺 New video: NEXO - Neural Network feat. LEXA</span> is now available",
+    "24 hours of non-stop music<span> - Over 200 music videos, Over 80 artists"</span>,
     "<span>Press number 8 to access THE WALL. Press 7 to leave your message (Limited time only).</span>",
-    "<span>Subscribe</span> to the YouTube channel to stay up to date with the latest news",
+    "<span>Subscribe</span>to the YouTube channel to stay up to date with the latest news",
 ];
